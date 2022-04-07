311b55ac33626bdd6ee3caf9b6fd77bff30e6b0d
Yogapriya P <yogapriya.pappaian@solitontech.com>
2022-04-07 13:28:13 +0530

    git hooks
    Signed-off-by: Yogapriya P <yogapriya.pappaian@solitontech.com>

---
+++ src/app/download/download.component.spec.ts
@@ -0,0 +1,27 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatMenuModule } from '@angular/material/menu';
+
+import { DownloadComponent } from './download.component';
+
+describe('DownloadComponent', () => {
+  let component: DownloadComponent;
+  let fixture: ComponentFixture<DownloadComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports:[MatMenuModule],
+      declarations: [ DownloadComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DownloadComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
